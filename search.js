9cc407c15d9ab3297fef28daecfa9a97ee6e7fb2
sanjeet <kumarsaneet1990@gmail.com>
2017-12-02 11:25:05 +0530

    wip on search

--- search.js
+++ search.js
@@ -1,7 +1,7 @@
 var async = require("async"),
-    request = require("request"),
-    _ = require("lodash"),
-    elasticsearch = require('elasticsearch');
+  request = require("request"),
+  _ = require("lodash"),
+  elasticsearch = require('elasticsearch');
 
 var es_host = "http://localhost:9200/";
 
@@ -10,90 +10,87 @@ var client = new elasticsearch.Client({
   // log: 'trace'
 });
 
+function main() {
 
-function main() {  
-  
   var es_index = "test";
   var es_index_type = "test_type";
 
-  
-  // 1. Age: > 25 & < 30  
-  
-  var es_query = {
-    "query": {
-        "bool": {
-          "must": [
-            {"range": {"age": {"gt": 25, "lt": 30} } }
-          ]
+  function es_search(es_query, search_name) {
+    client
+      .search({
+        index: es_index,
+        type: es_index_type,
+        body: es_query
+      }, function (err, resp, status) {
+        if (err) {
+          console.log("error", err)
+        } else {
+          console.log("Result@" + search_name + ":", resp.hits.hits);
         }
-      }, 
-      "from": 0, "size": 1000
-    };
+      });
+  }
 
-  client.search({
-      index : es_index,
-      type : es_index_type,
-      body : es_query
-    },function(err, resp, status){
-      if(err){
-        console.log("error",err)
+  // 1. Age: > 25 & < 30
+  var es_age_query = {
+    "query": {
+      "bool": {
+        "must": [
+          {
+            "range": {
+              "age": {
+                "gt": 25,
+                "lt": 30
+              }
+            }
+          }
+        ]
       }
-      else{
-        console.log("Result: ", resp.hits.hits);      
+    },
+    "from": 0,
+    "size": 1000
+  };
+
+  es_search(es_age_query, 'age');
+
+  // 2. Name: Sanjeet
+  var user_name = "Sanjeet"
+  var es_name_query = {
+    "query": {
+      "bool": {
+        "must": [
+          {
+            "term": {
+              "name.keyword": user_name
+            }
+          }
+        ]
       }
-    });
+    },
+    "from": 0,
+    "size": 1000
+  };
+
+  es_search(es_name_query, 'name');
+
+  // 3. Match all
+  var es_match_all_query = {
+    "query": {
+      "bool": {
+        "must": [
+          {
+            "match_all": {}
+          }
+        ]
+      }
+    },
+    "from": 0,
+    "size": 1000
+  };
+
+  es_search(es_match_all_query, 'matchAll');
 
 }
 
 main();
 
-console.log("Search!");
-
-
-// python code
-
-// import requests
-// import json
-
-// data = [{"name": "Ramanan", "age": 28}, {"name": "Sanjeet", "age": 35}]
-
-// es_host = "http://localhost:9200"
-// es_index = "test"
-// es_index_type = "test_type"
-
-
-// def python_requests(request_method, url, data=None, timeout=None, headers=None, auth=None):
-//     return requests.request(method=request_method, url=url, data=data, timeout=timeout, headers=headers, auth=auth)
-
-
-// # Delete if already exists
-// python_requests("DELETE", es_host + "/" + es_index)
-
-// # Create & put mapping
-// mappings = {"mappings": {es_index_type: {"properties": {"name": {"type": "text", "fields": {"keyword": {
-//     "ignore_above": 256, "type": "keyword"}}, "age": {"type": "long"}}}}}}
-// # http://localhost:9200/test
-// python_requests("PUT", es_host + "/" + es_index, data=json.dumps(mappings))
-
-// # Index data
-// for i in data:
-//     # http://localhost:9200/test/test_type
-//     python_requests("POST", es_host + "/" + es_index + "/" + es_index_type,
-//                     headers={"Content-Type": "application/json"},
-//                     data=json.dumps(i))
-
-// # Query data
-// search_uri = es_host + "/" + es_index + "/" + es_index_type + "/_search"
-
-// # 1. Age: > 25 & < 30
-// query = {"query": {"bool": {"must": [{"range": {"age": {"gt": 25, "lt": 30}}}]}}, "from": 0, "size": 1000}
-// print(python_requests("POST", search_uri, data=json.dumps(query)).text)
-
-// # 2. Name: Sanjeet
-// query = {"query": {"bool": {"must": [{"term": {"name.keyword": "Sanjeet"}}]}}, "from": 0, "size": 1000}
-// print(python_requests("POST", search_uri, data=json.dumps(query)).text)
-
-// # 3. Match all
-// query = {"query": {"bool": {"must": [{"match_all": {}}]}}, "from": 0, "size": 1000}
-// print(python_requests("POST", search_uri, data=json.dumps(query)).text)
-
+console.log("Searching!");
